3d044e9142eaa019059b0615044063fbd124c1d0
ChaitanyaDeepakTawade <138999096+ChaitanyaDeepakTawade@users.noreply.github.com>
2023-09-29 12:25:06 +0530

    Update Footer_Follow_Us_StepDefination.java

--- src/test/java/com/StepDefinations/Footer_Follow_Us_StepDefination.java
+++ src/test/java/com/StepDefinations/Footer_Follow_Us_StepDefination.java
@@ -22,7 +22,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class FooterFollowUsStepDefinition {
+public class Footer_Follow_Us_StepDefination {
 	
 	Footer_Follow_Us_Actions FooterObj = new Footer_Follow_Us_Actions();
 	
